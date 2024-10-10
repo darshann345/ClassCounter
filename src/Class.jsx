b184a6925906451dd11a810c7b6b9f9b279f4518
Darshan <darshann679596@gmail.com>
2024-10-10 23:00:11 +0530

    solved failed test cases

--- src/Class.jsx
+++ src/Class.jsx
@@ -1,29 +1,36 @@
-import React , {Component} from "react";
-class Class extends Component {
-    constructor(props){
-        super(props)
+import React, { Component } from "react";
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
-            count : 0,
-        }
+            count: 0,
+        };
     }
+
     increment = () => {
-        this.setState({count: this.state.count + 1})
-    }
+        this.setState({ count: this.state.count + 1 });
+    };
+
     decrement = () => {
-        this.setState({count: this.state.count - 1})
-    }
-    render(){
-        return(
+        this.setState({ count: this.state.count - 1 });
+    };
+
+    render() {
+        return (
             <>
                 <div>
                     <h1>Counter App</h1>
-                    <br/>
-                    <p>Count : {this.state.count}</p>
-                    <button onClick ={this.increment}>Increment</button>
-                    <button onClick={this.decrement}>Decrement</button>
+                    <br />
+                    <p>Count: {this.state.count}</p>
+                    <button onClick={this.increment}>Increment</button>
+                    <button onClick={this.decrement} disabled={this.state.count <= 0}>
+                        Decrement
+                    </button>
                 </div>
             </>
-        )
+        );
     }
 }
-export default Class;
+
+export default Counter;
