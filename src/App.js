9f23d83ffeee81877a0a11b1d392a531383c05ca
darshann345 <134913449+darshann345@users.noreply.github.com>
2025-01-10 16:12:30 +0530

    Update App.js
    class component counter app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import './App.css';
-import Class from './Class';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <>
-      <Class />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  decrement = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+
+  
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Counter App</h1>
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement} style={{ margin: '0 10px' }}>
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
